b85e19366105b030b648cbd9c8442a06cb736ba1
chrisramos25 <133292052+chrisramos25@users.noreply.github.com>
2025-12-13 23:55:45 -0600

    12/13/25 11 50

--- MenuPort1/guessGame.cpp
+++ MenuPort1/guessGame.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "guessGame.h"
+#include <cstdlib> //allows for srand and rand functions to be used
+#include <ctime>
 
 using namespace std;
 
 void playGuessingGame() {
-    cout << "GUESSING GAME STARTED";
+    cout << "GUESSING GAME STARTED...";
+
+	srand(time(0)); //starting (seed) for random numbers with rand() (time(0) so that it gives a different seed each time)
+	cout << rand() % 100 + 1; //generates random number between 1 and 100
+
 }
---
+++ howTo.md
@@ -0,0 +1,15 @@
+`git status`
+
+# 1. Stage the new file
+`git add howTo.md` or 
+`git add`
+
+# 2. Commit the staged file with a message
+`git commit` or 
+`git commit -m "your message"`
+
+# 3. Push the commit to GitHub
+git push
+
+
+ 
